27b1006c2a57e56394a4a2c3a11a1400c61ce3d4
yinjun622 <yinjun622@163.com>
2014-10-29 11:45:56 +0800

    将 《和》改为跳转到第一页和最后一页
    onPageBtnClick 方法中添加了对《和》的判断

--- jBootstrapPage.js
+++ jBootstrapPage.js
@@ -28,6 +28,8 @@
             function init() {
             	$this.find('li').remove();
             	c.maxPages = Math.ceil(c.total/c.pageSize);
+
+                c.maxPageButton = c.maxPages < c.maxPageButton ? c.maxPages : c.maxPageButton;
             	
             	if(c.maxPages < 1) return; 
             	
@@ -139,40 +141,7 @@
             		
             		bindPages();
             	}
-            	
-            	
-            	
-            	
-            	/*if((currButtonNum != currPage+1 || maxPage > showPage) && currButtonNum > middle) {
-            		var startPages = currPage - middle + offsetRight;
-            		
-            		var endPages = currPage + middle + offsetRight;
-            		endPages = endPages > maxPage ? maxPage : endPages;
-            		
-            		if(endPages < c.maxPageButton) endPages = c.maxPageButton;
-            		
-            		if(endPages - startPages > c.maxPageButton) {
-            			var d = endPages - startPages - c.maxPageButton;
-            			endPages -= d;
-            		} 
-            		
-            		if(endPages - startPages < c.maxPageButton) {
-            			var d = c.maxPageButton - (endPages - startPages); 
-            			startPages -= d;
-            		}
-            		
-            		var pNum = 0;
-            		var html = '';
-            		for(var index = startPages; index < endPages; index++) {
-            			pNum++;
-            			html += '<li class="page" pNum="'+pNum+'"><a href="#" page="'+index+'">'+index+'</a></li>';
-            		}
-            		
-            		$this.find('li.page').remove();
-            		$this.find('li:nth-child(2)').after(html);
-            		
-            		bindPages();
-            	}*/
+            
             }
             
             function onClickPage(pageBtn) {
@@ -219,9 +188,10 @@
             	var selectedText = $_this.text();
             	var selectedBtn = $this.find('li.active').find('a');
             	
-            	if(selectedText == '下一页' || selectedText == '»') {
+            	if(selectedText == '下一页') {
             		
             		var selectedIndex = parseInt(selectedBtn.text());
+
             		var selectNum = parseInt($this.find('li.active').attr('pNum'))+1;
             		if(selectNum > c.maxPageButton) selectNum = c.maxPageButton-1;
             		
@@ -230,14 +200,22 @@
             			selectedBtn = $this.find('li.page').find('a[page="'+(selectedIndex+1)+'"]');
             		}
             	}
-            	else if(selectedText == '上一页'  || selectedText == '«') {
+            	else if(selectedText == '上一页') {
             		var selectedIndex = parseInt(selectedBtn.text())-1;
             		var selectNum = parseInt($this.find('li.active').attr('pNum'))-1;
             		if(selectNum < 1) selectNum = 1;
-            		
             		mathPrePage(selectNum, selectedIndex, c.maxPages, c.maxPageButton);
             		selectedBtn = $this.find('li.page').find('a[page="'+(selectedIndex)+'"]');
             	}
+                else if(selectedText == '»'){
+
+                    mathNextPage(c.maxPageButton-1, c.maxPages, c.maxPages, c.maxPageButton);
+                    selectedBtn = $this.find('li.page').find('a[page="'+(c.maxPages)+'"]');
+                }
+                else if(selectedText == '«'){
+                    mathPrePage(2, 1, c.maxPages, c.maxPageButton);
+                    selectedBtn = $this.find('li.page').find('a[page="'+(1)+'"]');
+                }
             	else {
             		selectedBtn = $_this;
             	}
